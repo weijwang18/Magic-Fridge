d04cb75682ba2e98608f47ad8975cdef70045fe1
zbl14 <ifthereisoneday@gmail.com>
2022-07-05 15:35:25 -0700

    update the parameter for API call

--- src/services/recipes-service.js
+++ src/services/recipes-service.js
@@ -1,7 +1,7 @@
 export default class RecipeService {
   static async getRecipe(ingredient, health, cuisineType, mealType) {
     try {
-      const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${ingredient}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_API_KEY}&diet=${diet}&health=${health}&cuisineType=${cuisineType}&mealType=${mealType}`);
+      const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${ingredient}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_API_KEY}&health=${health}&cuisineType=${cuisineType}&mealType=${mealType}`);
       if (!response.ok) {
         throw Error(response.statusText);
       }
