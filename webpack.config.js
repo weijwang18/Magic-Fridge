a9aaf112b3d5b59009af89cf9b0f7af7c5ac2772
weijwang18@gmail.com <weijwang18@gmail.com>
2022-07-05 13:41:08 -0700

    add sections for vegetables, beef, pork, chicken

--- webpack.config.js
+++ webpack.config.js
@@ -4,56 +4,51 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  devtool: 'eval-source-map',
-  devServer: {               
-    contentBase: './dist'    
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: "./dist",
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new Dotenv(), // new line
     new HtmlWebpackPlugin({
-      title: '------',
-      template: './src/index.html',
-      inject: 'body'
-    })
-    new Dotenv()
+      title: "currency-exchanger",
+      template: "./src/index.html",
+      inject: "body",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "eslint-loader"
+        loader: "eslint-loader",
       },
       {
         test: /\.(gif|png|avif|jpe?g)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/images/'
-            }
-          }
-        ]
+              name: "[name].[ext]",
+              outputPath: "assets/images/",
+            },
+          },
+        ],
       },
       {
-        test:/\.html$/,
-        use: [
-          'html-loader'
-        ]
+        test: /\.html$/,
+        use: ["html-loader"],
       },
-    ]
-  }
+    ],
+  },
 };
