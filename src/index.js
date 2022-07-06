06b47f7ee10accf1e6f2f832cf6c49fe54a68786
weijwang18@gmail.com <weijwang18@gmail.com>
2022-07-06 11:57:20 -0700

    add CSS styles
    Co-authored-by: Ryan DeFea <ryan.defea@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-import RecipeService from './services/recipes-service';
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+import RecipeService from "./services/recipes-service";
 
 let ingredient = "chicken";
 let health = "gluten-free";
@@ -10,35 +10,76 @@ let cuisineType = "American";
 let mealType = "Breakfast";
 
 async function makeApiCall() {
-  const response = await RecipeService.getRecipe(ingredient, health, cuisineType, mealType);
+  const response = await RecipeService.getRecipe(
+    ingredient,
+    health,
+    cuisineType,
+    mealType
+  );
   getElements(response);
 }
 
 function getElements(response) {
   if (response.hits) {
     for (let i = 0; i < response.hits.length; i++) {
-      $('.showRecipes').append(`
+      $(".showRecipes").append(`
       <div class="card" style="width: 20rem;">
-        <img src="${response.hits[i].recipe.images.REGULAR.url}" class="card-img-top" alt="a photo of meal">
+        <img src="${
+          response.hits[i].recipe.images.REGULAR.url
+        }" class="card-img-top" alt="a photo of meal">
         <div class="card-body">
           <h5 class="card-title">${response.hits[i].recipe.label}</h5>
-          <p class="card-text">Total time: ${response.hits[i].recipe.totalTime} mins</p>
-          <p class="card-text">Total calories: ${(parseFloat(response.hits[i].recipe.calories).toFixed(2))}</p>
+          <p class="card-text">Total time: ${
+            response.hits[i].recipe.totalTime
+          } mins</p>
+          <p class="card-text">Total calories: ${parseFloat(
+            response.hits[i].recipe.calories
+          ).toFixed(2)}</p>
           <div id="showIngredient${i}"> 
           </div>  
-          <a href="${response.hits[i].recipe.url}" class="btn btn-primary center">See full recipe</a>
+          <a href="${
+            response.hits[i].recipe.url
+          }" class="btn btn-primary center">See full recipe</a>
         </div>
       </div>
       `);
-      let ingredientArr =  response.hits[i].recipe.ingredientLines;
-      let ingredientStr = `<ul><li> ${ingredientArr.join("</li><li>")} </li></ul>`;
+      let ingredientArr = response.hits[i].recipe.ingredientLines;
+      let ingredientStr = `<ul><li> ${ingredientArr.join(
+        "</li><li>"
+      )} </li></ul>`;
       $(`#showIngredient${i}`).append(`${ingredientStr}`);
     }
   } else {
-    $('.showErrors').text(`There was an error processing your request: ${response}`);
+    $(".showErrors").text(
+      `There was an error processing your request: ${response}`
+    );
   }
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
+  $("h3#Vegetable").click(function () {
+    $(".vegetables").slideToggle();
+  });
+  $("h3#Beef").click(function () {
+    $(".beef").slideToggle();
+  });
+  $("h3#Pork").click(function () {
+    $(".pork").slideToggle();
+  });
+  $("h3#Poultry").click(function () {
+    $(".poultry").slideToggle();
+  });
+  $("h3#Fish").click(function () {
+    $(".fish").slideToggle();
+  });
+  $("h3#ShellFish").click(function () {
+    $(".shellfish").slideToggle();
+  });
+  $("h3#Dairy").click(function () {
+    $(".dairyProducts").slideToggle();
+  });
+  $("h3#Grain").click(function () {
+    $(".grain").slideToggle();
+  });
   makeApiCall();
 });
