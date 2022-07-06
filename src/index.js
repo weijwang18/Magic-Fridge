92ff404c490008cf330e80c8741e613f934324f0
zbl14 <ifthereisoneday@gmail.com>
2022-07-06 16:45:55 -0700

    throw error message when not find a recipe based on the selection
    Co-authored-by: Jonathan Lu <jdlu.coding@gmail.com>
    Co-authored-by: Joseph Jack <joseph.w.jack@gmail.com>
    Co-authored-by: Ryan DeFea <ryan.defea@gmail.com>
    Co-authored-by: Winnie Wang <weijwang18@gmail.com>

--- src/index.js
+++ src/index.js
@@ -19,32 +19,38 @@ async function makeApiCall() {
 
 function getElements(response) {
   if (response.hits) {
-    for (let i = 0; i < response.hits.length; i++) {
-      $(".showRecipes").append(`
-      <div class="card" style="width: 20rem;">
-        <img src="${response.hits[i].recipe.images.REGULAR.url}" class="card-img-top" alt="a photo of meal">
-        <div class="card-body">
-          <h5 class="card-title">${response.hits[i].recipe.label}</h5>
-          <p class="card-text">Total time: ${response.hits[i].recipe.totalTime} mins</p>
-          <p class="card-text">Total calories: ${(parseFloat(response.hits[i].recipe.calories).toFixed(2))}</p>
-          <p class="card-text">Total Fat: ${(parseInt(response.hits[i].recipe.totalNutrients.FAT.quantity))}${response.hits[i].recipe.totalNutrients.FAT.unit}</p>
-          <p class="card-text">Total Carbs: ${(parseInt(response.hits[i].recipe.totalNutrients.CHOCDF.quantity))}${response.hits[i].recipe.totalNutrients.CHOCDF.unit}</p>
-          <p class="card-text">Total Protein: ${(parseInt(response.hits[i].recipe.totalNutrients.PROCNT.quantity))}${response.hits[i].recipe.totalNutrients.PROCNT.unit}</p>
-          <div class="ingredientListTitle">
-            <p>Ingredient List</p>
-            <div id="showIngredient${i}" class="ingredientList"> 
-            </div>
-          </div>  
-          <a href="${response.hits[i].recipe.url}" 
-          class="btn btn-primary center">See full recipe</a>
+    if (response.hits.length == 0) {
+      $(".showErrors").text(
+        `Sorry, we couldn't find a recipe based on your selection. Please try something else. `
+      );
+    } else {
+      for (let i = 0; i < response.hits.length; i++) {
+        $(".showRecipes").append(`
+        <div class="card" style="width: 20rem;">
+          <img src="${response.hits[i].recipe.images.REGULAR.url}" class="card-img-top" alt="a photo of meal">
+          <div class="card-body">
+            <h5 class="card-title">${response.hits[i].recipe.label}</h5>
+            <p class="card-text">Total time: ${response.hits[i].recipe.totalTime} mins</p>
+            <p class="card-text">Total calories: ${(parseFloat(response.hits[i].recipe.calories).toFixed(2))}</p>
+            <p class="card-text">Total Fat: ${(parseInt(response.hits[i].recipe.totalNutrients.FAT.quantity))}${response.hits[i].recipe.totalNutrients.FAT.unit}</p>
+            <p class="card-text">Total Carbs: ${(parseInt(response.hits[i].recipe.totalNutrients.CHOCDF.quantity))}${response.hits[i].recipe.totalNutrients.CHOCDF.unit}</p>
+            <p class="card-text">Total Protein: ${(parseInt(response.hits[i].recipe.totalNutrients.PROCNT.quantity))}${response.hits[i].recipe.totalNutrients.PROCNT.unit}</p>
+            <div class="ingredientListTitle">
+              <p>Ingredient List</p>
+              <div id="showIngredient${i}" class="ingredientList"> 
+              </div>
+            </div>  
+            <a href="${response.hits[i].recipe.url}" 
+            class="btn btn-primary center">See full recipe</a>
+          </div>
         </div>
-      </div>
-      `);
-      let ingredientArr = response.hits[i].recipe.ingredientLines;
-      let ingredientStr = `<ul><li> ${ingredientArr.join(
-        "</li><li>"
-      )} </li></ul>`;
-      $(`#showIngredient${i}`).append(`${ingredientStr}`);
+        `);
+        let ingredientArr = response.hits[i].recipe.ingredientLines;
+        let ingredientStr = `<ul><li> ${ingredientArr.join(
+          "</li><li>"
+        )} </li></ul>`;
+        $(`#showIngredient${i}`).append(`${ingredientStr}`);
+      }
     }
   } else {
     $(".showErrors").text(
@@ -94,7 +100,7 @@ $(document).ready(function () {
       cuisineType = $("#cuisineType").val();
       mealType = $("#mealType").val();
     });
-    // console.log(ingredient);
+    console.log(ingredient);
     makeApiCall(); 
     clearFields();
   });
