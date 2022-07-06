1ba4650408afd8d0a58b11deff3354224606bb94
zbl14 <ifthereisoneday@gmail.com>
2022-07-05 17:27:35 -0700

    update the enviroment

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
-  devtool: "eval-source-map",
-  devServer: {
-    contentBase: "./dist",
+  devtool: 'eval-source-map',  
+  devServer: {                 
+    contentBase: './dist'      
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Magic-Fridge',
+      title: 'Magic Fridge',
       template: './src/index.html',
       inject: 'body'
     }),
@@ -26,29 +26,16 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "eslint-loader",
-      },
-      {
-        test: /\.(gif|png|avif|jpe?g)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "assets/images/",
-            },
-          },
-        ],
-      },
-      {
-        test: /\.html$/,
-        use: ["html-loader"],
-      },
-    ],
-  },
+        loader: "eslint-loader"
+      }
+    ]
+  }
 };
